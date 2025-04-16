7fbf79ccb1b0ccb1c008b7a8b4fcb86ec00808f9
Vedansh Mishra~ <vedanshmishra90@gmail.com~>
2025-04-17 02:58:04 +0530

    bug fix

--- ws_client_camera.py
+++ ws_client_camera.py
@@ -6,25 +6,22 @@ import redis
 import base64
 
 async def receive_frames():
-    uri = "ws://https://raspberrypi3b.tail966e7e.ts.net/camera"
+    uri = "ws://100.113.195.60:8761"
     redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
     try:
         async with websockets.connect(uri) as websocket:
             print(f"Connected to server at {uri}")
             while True:
-                frame_bytes = await websocket.recv()
-                frame = np.frombuffer(frame_bytes, dtype=np.uint8)
-                frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+                frame_b64 = await websocket.recv()  # receive as base64 string
+                frame_bytes = base64.b64decode(frame_b64)
+                frame_np = np.frombuffer(frame_bytes, dtype=np.uint8)
+                frame = cv2.imdecode(frame_np, cv2.IMREAD_COLOR)
 
                 if frame is not None:
                     _, buffer = cv2.imencode('.jpg', frame)
                     encoded = base64.b64encode(buffer).decode('utf-8')
-
-                    channel = 'camera_frame'
-                    message = encoded.encode('utf-8')
-
-                    redis_client.set(channel, message)
+                    redis_client.set('camera_frame', encoded.encode('utf-8'))
 
                     cv2.imshow("Camera Feed", frame)
                     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -34,6 +31,7 @@ async def receive_frames():
         print(f"Connection error: {e}")
     finally:
         cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     try:
